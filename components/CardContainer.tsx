dd34c4dd259a74cc23fd85aac6e12d37541d71d5
Rocio Arias <roarias48@gmail.com>
2026-05-06 22:49:29 -0300

    Otro cambio

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -1,9 +1,18 @@
+import { IProduct } from "@/interfaces/product.interface";
 import Card from "./Card";
 import { getAllProducts } from "@/services/products.services";
 
 
+// async function CardContainer() {
+//   const allProducts = await getAllProducts();
 async function CardContainer() {
-  const allProducts = await getAllProducts();
+  let allProducts: IProduct[] = [];
+  
+  try {
+    allProducts = await getAllProducts();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
 
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9">
